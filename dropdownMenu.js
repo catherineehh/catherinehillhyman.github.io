77ba55ce03821fcc86a30fb4bfe6bae142aa77af
cehillhyman <47611047+cehillhyman@users.noreply.github.com>
2019-06-27 14:29:49 -0400

    Create dropdownMenu.js

---
+++ dropdownMenu.js
@@ -0,0 +1,16 @@
+function dropItDown(){
+document.getElementsByClassName("dropList").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if(!event.target.matches(".dropButton")) {
+    var dropdowns = document.getElementsByClassName("dropList");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropDown = dropdowns[i];
+      if (openDropdown.classList.contains("show")){
+        openDropdown.classList.remove("show");
+        }
+      }
+    }
+  }
