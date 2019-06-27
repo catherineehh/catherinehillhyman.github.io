79f3b4aa7ba9ea977327557fb3f100964f65a856
cehillhyman <47611047+cehillhyman@users.noreply.github.com>
2019-06-27 14:56:20 -0400

    Update dropdownMenu.js

--- dropdownMenu.js
+++ dropdownMenu.js
@@ -1,16 +1,3 @@
 function dropItDown(){
-document.getElementByID("cpt114").classList.toggle("show");
+document.getElementsByClassName("dropList").style.display = "block";
 }
-
-window.onclick = function(event) {
-  if(!event.target.matches(".dropButton")) {
-    var dropdowns = document.getElementsByClassName("dropList");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropDown = dropdowns[i];
-      if (openDropdown.classList.contains("show")){
-        openDropdown.classList.remove("show");
-        }
-      }
-    }
-  }
